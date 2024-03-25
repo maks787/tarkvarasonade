3930547ff667447af667be63c3a43cb342a8eda8
Maksym Miskevych <120181270+maks787@users.noreply.github.com>
2024-03-25 15:54:20 +0200

    Add files via upload

--- tarkvarascript.js
+++ tarkvarascript.js
@@ -28,11 +28,43 @@ const synad= {
     'puudused': 'недостатки',
     'elutsükel': 'каскадная модель',
     'arendamine': 'разработка',
-    'valideerimine': 'каскадная модельa',
+    'valideerimine': 'каскадная модель',
 
 
 
 }
+const vsynad = {
+    'реализация': 'teostus',
+    'план': 'kavand',
+    'алгоритм': 'algoritm',
+    'пользовательский интерфейс': 'kasutajaliides',
+    'модель': 'muudel',
+    'схема базы данных': 'andmebaasiskeem',
+    'стандарт': 'standart',
+    'цикл': 'tsükkel',
+    'обработка данных': 'andmetõõtlus',
+    'структура данных': 'andmestruktuur',
+    'среда разработки': 'arenduskeskkond',
+    'работник по управлению проектами': 'projektihaldus töörist',
+    'планирование': 'kavandamine',
+    'итерация': 'iteratsioon',
+    'исправление': 'parandus',
+    'каскадная модель': 'koskmudel',
+    'агильная модель': 'agiline mudel',
+    'спиральная модель': 'spiraalne mudel',
+    'инкрементальная модель': 'inkrementaalne mudel',
+    'требованя': 'nõudmised',
+    'каскадная модель': 'ealiseerimine',
+    'тестирование': 'testimine',
+    'интеграция': 'integreerimine',
+    'использование': 'kasutamine',
+    'поддержка': 'holdus',
+    'преимущества': 'eelised',
+    'недостатки': 'puudused',
+    'жизненный цикл': 'elutsükel',
+    'разработка': 'arendamine',
+    'проверка на правильность': 'valideerimine',
+};
 function randomSyna(){
 
     const keys = Object.keys(synad)
@@ -47,6 +79,18 @@ function randomSyna(){
 function sonakontroll(){
 
     const syna= document.getElementById("kontroll").value
-    document.getElementById("vastus").innerHTML = syna ===synad[document.getElementById("random-sona").innerHTML]
+    document.getElementById("vastus").innerHTML = syna ===synad[document.getElementById("random-sona").innerHTML] ? "Õige!" : "Vale!";
 
+}
+//uus funktsioonid
+function vrandomSyna(){
+    const keys = Object.keys(vsynad);
+    const vjuhuslikSyna = keys[Math.floor(Math.random() * keys.length)];
+
+    document.getElementById("vrandom-sona").innerHTML = vjuhuslikSyna;
+}
+
+function vsonakontroll(){
+    const syna = document.getElementById("vkontroll").value;
+    document.getElementById("vvastus").innerHTML = syna === vsynad[document.getElementById("vrandom-sona").innerHTML] ? "Õige!" : "Vale!";
 }
